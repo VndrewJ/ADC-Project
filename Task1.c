94c9e351045e1cf341b87d00a5b146602723fd5d
VndrewJ <andyjkoh@gmail.com>
2023-03-28 15:54:28 +1300

    added task 1 ADC stuff

--- Task1.c
+++ Task1.c
@@ -9,15 +9,32 @@ void main(){
 
     //pin setup
     DDRC &= ~(1<<PIN0);     //set pin A0 to read
-
-    
-
+    DDRB |= (1<<PIN1);      //Set Port B pin 1 to output
 
     //superloop
     while(1){
-        _delay_ms(1000);
-        uint16_t voltage;
-        voltage = ADC;
-        
+        //start conversion
+        ADCRSA |= (1<<ADSC);
+        while(ADCRSA & (1<<ADSC)){      //while converting, do nothing
+            asm("nop");
+        }
+        uint16_t voltage;               //initialise voltage
+        voltage = ADC;                  //get values
+
+        if(voltage >= 2){               //if voltage is above 2V turn on and off a couple times
+            PORTB |= (1<<PIN0);       
+            _delay_ms(50);
+            PORTB &= ~(1<<PIN0);
+            _delay_ms(50);  
+            PORTB |= (1<<PIN0);       
+            _delay_ms(50);
+            PORTB &= ~(1<<PIN0);
+            _delay_ms(50);  
+        }else{                          //else just do it once
+            PORTB |= (1<<PIN0);       
+            _delay_ms(50);
+            PORTB &= ~(1<<PIN0);
+            _delay_ms(50);  
+        }
     }
 }
