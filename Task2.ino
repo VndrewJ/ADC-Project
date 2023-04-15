98f98d2de0ff84054a8cbcc5b7e6f62b38f9ad62
VndrewJ <andyjkoh@gmail.com>
2023-04-15 19:48:59 +1200

    not working task 2

--- Task2.ino
+++ Task2.ino
@@ -55,7 +55,71 @@ int main(void){
 
     //superloop
     while(1){
-
     }
+}
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <inttypes.h>
+#include <util/delay.h>
+
+//Macros for robustness (add later)
+
+int ringBuffer[1000];
+int i=0;
+
+//timer interrupt
+ISR(TIMER1_COMPA_vect){
+    ADCSRA |= (1<<ADSC);                                            //start conversion
+    while(ADCSRA & (1<<ADSC)){
+      asm("nop");
+    }
+    /*
+    uint16_t volatile voltage = ADCH;
+    ringBuffer[i%1000] = ADCH;
+    Serial.println((String)"At index " +i%1000+ " is " +ADCH);
+    i++;
+    */
+  
+  ringBuffer[i%1000] = ADCH;
+  i++;
+
+  if (i%1000 == 0){
+    for (int j = 0; j < 1000; j++){
+      Serial.println((String)"The data at " +j+ " is " +(ringBuffer[j]));
+    }
+  }
     
 }
+
+// // //ADC completion interrupt
+// // ISR(ADC_vect){
+    
+// // }
+
+// int main(void){
+//     Serial.begin(9600);
+//     cli(); 
+
+//     //ADC setup                                                       
+//     //ADMUX |= (1<<ADLAR);                                            //left shift for 8 bit results   
+//     //DDRB |= (1<<PIN5);                                              //Set Port B pin 5 to output
+
+//     //enable ADC and interrupts, then set prescaler to 128
+//     ADCSRA |= ((1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0));  
+//     ADMUX &= ~((1<<REFS1) | (1<<REFS0) | (1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0));
+//     ADMUX |= (1<<ADLAR);
+
+//     //timer setup
+//     TCCR1B |= (1<<WGM12) | (1<<CS12) | (1<<CS10);                   //enable CTC mode and set prescaler to 64
+//     OCR1A = 78;                                                   //set compare value to 5ms
+//     TIMSK1 |= (1 << OCIE1A);                                        // Enable CTC interrupt for OCR1A compare match
+
+//     sei();
+
+//     //superloop
+//     while(1){
+//         asm("nop");
+//     }
+    
+// }
