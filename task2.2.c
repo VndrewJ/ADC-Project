fc6a741b4af0250a5a42fb46b8f777c3264fc831
jnor758 <jnor758@aucklanduni.ac.nz>
2023-04-27 20:59:08 +1200

    Added sampling rate check

--- task2.2.c
+++ task2.2.c
@@ -8,16 +8,28 @@
 
 volatile uint8_t ringBuffer[RINGBUFFER_SIZE];
 int i=0;
+int counter = 0;
 
 //timer interrupt
 ISR(TIMER1_COMPA_vect){
+
+    counter++;
+
+    /*
+    //Function used to validate 200SA/s
+    if(counter == 200){
+      PORTB ^= (1<<PIN4);
+      counter = 0;
+    }
+    */
+
     ADCSRA |= (1<<ADSC);                                            //start conversion
 }
 
 //ADC completion interrupt
 ISR(ADC_vect){
     ringBuffer[i%RINGBUFFER_SIZE] = ADCH;          //add into ring buffer
-  	Serial.println((String)"At index " +i%RINGBUFFER_SIZE+ " the voltage is " +ringBuffer[i%RINGBUFFER_SIZE]);    //print value at i in ring buffer
+    Serial.println((String)"At index " +i%RINGBUFFER_SIZE+ " the voltage is " +ringBuffer[i%RINGBUFFER_SIZE]);    //print value at i in ring buffer
     i++;
 }
 
@@ -35,6 +47,8 @@ int main(void){
     TCCR1B |= (1<<WGM12) | (1<<CS11) | (1<<CS10);                   //enable CTC mode and set prescaler to 64
     OCR1A = 1247;                                                   //set compare value to 5ms
     TIMSK1 |= (1 << OCIE1A);                                        // Enable CTC interrupt for OCR1A compare match
+
+    DDRB |= (1<<PIN4);                                              //Setting pin 4 to output for sampling rate demonstration
 
     sei();
 
